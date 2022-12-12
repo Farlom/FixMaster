2d27a0c42c839b7eb0f7250e21ad51bf2191f69e
Katharsist <rifase665@gmail.com>
2022-12-12 11:30:32 +0300

    Add salary classes

--- FixMasterProject/fixmaster.cpp
+++ FixMasterProject/fixmaster.cpp
@@ -109,3 +109,157 @@ void MasterInputScreen::setMaster()
     ptrMasterList->instertMaster(ptrMaster);
 }
 
+//методы класса SalaryRow
+SalaryRow::SalaryRow(string n): masterName(n)
+{
+    fill(&salary[0], &salary[12], 0); //заполняем массив нулями
+
+}
+
+void SalaryRow::setSalary(int month)
+{
+    salary[month-1]++;
+}
+
+string SalaryRow::getMasterName()
+{
+    return masterName;
+}
+
+int SalaryRow::getSalaryFor(int month)
+{
+    return salary[month];
+}
+
+int SalaryRow::getSumOfSalary()
+{
+    int tmp = 0;
+    for (int i = 0; i < 12; i++)
+    {
+        tmp += salary[i];
+    }
+    return tmp;
+}
+
+
+//методы класса SalaryRecord
+SalaryRecord::~SalaryRecord()
+{
+    while(!setPtrsSR.empty())
+    {
+        iter = setPtrsSR.begin();
+        delete *iter;
+        setPtrsSR.erase(iter);
+    }
+}
+
+void SalaryRecord::insertSalary(string masterName, int month)
+{
+    /*
+     * инкрементирует счетчик работ у мастера masterName в месяце month
+     */
+    iter = setPtrsSR.begin();
+    while(iter != setPtrsSR.end())
+    {
+        if (masterName == (*iter)->getMasterName())
+        {
+            (*iter)->setSalary(month);
+            return;
+        }
+        else
+            iter++;
+    }
+    SalaryRow* ptrRow = new SalaryRow(masterName);
+    ptrRow->setSalary(month);
+    setPtrsSR.push_back(ptrRow);
+}
+
+void SalaryRecord::display()
+{
+    wcout << L"Зарплатная ведомость\n";
+
+    iter = setPtrsSR.begin();
+    if(setPtrsSR.begin() == setPtrsSR.end())
+    {
+        wcout << L"Список пуст.\nДля продолжения нажмите любую клавишу...";
+        getch();
+        return;
+    }
+    else
+    {
+        wcout << L"Имя\tЯнварь\tФевраль\tМарт\tАпрель\tМай\tИюнь\tИюль\tАвгуст\tСент-рь\tОктябрь\tНоябрь\tДекабрь\tИтого\n";
+        while(iter != setPtrsSR.end())
+        {
+
+            cout<<(*iter)->getMasterName();
+
+            for(int i = 0; i < 12; i++)
+            {
+                cout<<"\t"<<(*iter)->getSalaryFor(i);
+            }
+
+            cout<<"\t"<<(*iter)->getSumOfSalary()<<"\n";
+
+            iter++;
+        }
+    }
+    //(*iter)->getMasterName();
+
+          //TODO
+    //выводит таблицу "Зарплатная ведомость" (возможно, еще придется умножать на зп мастера)
+}
+
+int SalaryRecord::getSumOfSalaries()
+{
+    int sum = 0;
+    iter = setPtrsSR.begin();
+    while(iter != setPtrsSR.end())
+    {
+        sum = sum + (*iter)->getSumOfSalary();
+        iter++;
+    }
+
+    return sum;
+    //TODO
+    //выводит сумму всех оплат мастерам
+}
+
+
+//методы класса SalaryInputScreen
+void SalaryInputScreen::setSalary()
+{
+    //wcout<< L"SalaryInputScreen.setSalary() \n";
+
+    if (!ptrMasterList->display())
+    {
+        return;
+    }
+    else {
+
+        wcout<<L"Введите имя мастера: ";
+
+        cin >> masterName;
+        if (ptrMasterList->getSal(masterName) == -1)
+        {
+            wcout << L"Такого мастера нет в списке!";
+            getch();
+            system("cls");
+            return;
+        }
+        else
+        {
+            wcout << L"\nВведите месяц, в котором выполнялись работы: ";
+            cin >> month;
+            ptrSalaryRecord->insertSalary(masterName, month);
+        }
+
+    }
+
+    //TODO
+    /* получить имя мастера
+     * если имя найдено (и такой мастер существует)
+     * инкрементируем
+     */
+}
+
+
